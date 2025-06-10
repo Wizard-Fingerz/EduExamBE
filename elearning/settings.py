816819ed22b42314ab896c777a7493dd0bd0d7fd
WizCode <adewale.oladiti28@gmail.com>
2025-06-10 11:19:24 +0100

    Refactor static files configuration in settings.py and update vercel.json routes for improved deployment. Adjusted STATICFILES_STORAGE based on DEBUG mode and corrected static file paths.

--- elearning/settings.py
+++ elearning/settings.py
@@ -147,8 +147,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# Configure static files storage for Vercel
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
