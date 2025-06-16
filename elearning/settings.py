1406b4a52b9960acd73d1ea5a7da0bd0839bbe4b
WizCode <adewale.oladiti28@gmail.com>
2025-06-16 18:35:26 +0100

    Update Course model to allow nullable fields for price, category, and level; change duration field to IntegerField with a default value. Enhance migration files to reflect these changes and establish relationships for CourseEnrollment and CourseRating models.

--- elearning/settings.py
+++ elearning/settings.py
@@ -211,6 +211,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://adapt-learn-three.vercel.app",
     "http://localhost:5174",
+    "http://localhost:5173",
 ]
 
 # Additional CORS settings
